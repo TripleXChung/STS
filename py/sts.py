e0ee34a4a60161fabebef7b9b057d3e4e4a508e9
TripleXChung <xxx.phy@gmail.com>
2013-01-21 01:29:22 +0800

    Multiprocess version
    WIthout persistent 8000+ req/s. With persistent 5000+ req/s.

--- py/sts.py
+++ py/sts.py
@@ -3,102 +3,9 @@ import redis
 import time
 import datetime
 import collections
+from multiprocessing import Process, Pipe
 import cPickle as pickle
 import simplejson as json
-
-datas = {}
-totals = {}
-persistent = False
-client = redis.Redis(host='localhost', port=6379, db=0)
-
-def publish_avg(server, timestamp, avg):
-	global client
-	msg = json.dumps([server, timestamp, avg])
-	client.publish('avg', msg)
-	#print msg
-
-def load_data(server):
-	global datas
-	if(not datas.has_key(server)):
-		datas[server] = collections.deque([])
-		totals[server] = 0
-	return datas[server]
-
-def load_persistent_data(server):
-	global client
-	if(datas.has_key(server)):
-		return datas[server]
-	l = client.llen(server)
-	res = collections.deque([])
-	total = 0
-	if(l > 0):
-		tmps = client.lrange(server, 0, l)
-		for tmp in tmps:
-			obj = pickle.loads(tmp)
-			res.append(obj)
-			total = total + obj[1]
-	totals[server] = total
-	datas[server] = res
-	return res
-
-def apply_value(server, timestamp, value):
-	global client
-	global persistent
-	if(type(server) != int):
-		return
-	#server_string = "%2.2x"%(server)
-
-	if(type(timestamp) != int):
-		return
-	if(type(value) != int):
-		return
-	obj = [timestamp, value]
-	
-	if(persistent):
-		data = load_persistent_data(server)
-	else:
-		data = load_data(server)
-
-	data.append(obj)
-	totals[server] = totals[server] + obj[1]
-	total = totals[server]
-
-	if(persistent):
-		client.rpush(server, pickle.dumps(obj))
-	"""new_data = []
-	last_timestamp = timestamp
-	for obj in data:
-		timestamp = obj[0]
-		if(last_timestamp - timestamp < (30 * 24 * 3600)):
-			new_data.append(obj)
-	data = new_data"""
-	
-	last_timestamp = timestamp
-	while True:
-		obj = data[0]
-		timestamp = obj[0]
-		if(last_timestamp - timestamp > (30 * 24 * 3600)):
-			data.popleft()
-			if(persistent):
-				client.lpop(server)
-			totals[server] = totals[server] - obj[1]
-			total = totals[server]
-		else:
-			break
-	count = len(datas)
-	avg = total / count
-	#print server_string + '--' + str(count)
-	publish_avg(server, last_timestamp, avg)
-
-def handle_request(req):
-	if(type(req) != type([])):
-		return
-	if(len(req) != 3):
-		return
-	server = req[0]
-	timestamp = req[1]
-	value = req[2]
-	apply_value(server, timestamp, value)
 
 profile_count = 0 
 profile_timestamp = 0
@@ -114,7 +21,108 @@ def profile():
 	print (profile_count / timediff)
 	profile_count = 0
 
+datas = {}
+totals = {}
+persistent = False
+
+class STS():
+	def __init__(self):
+		self.client = redis.Redis(host='localhost', port=6379, db=0)
+
+	def publish_avg(self, server, timestamp, avg):
+		msg = json.dumps([server, timestamp, avg])
+		self.client.publish('avg', msg)
+		profile()
+		#print msg
+
+	def load_data(self, server):
+		global datas
+		if(not datas.has_key(server)):
+			datas[server] = collections.deque([])
+			totals[server] = 0
+		return datas[server]
+
+	def load_persistent_data(self, server):
+		if(datas.has_key(server)):
+			return datas[server]
+		l = self.client.llen(server)
+		res = collections.deque([])
+		total = 0
+		if(l > 0):
+			tmps = self.client.lrange(server, 0, l)
+			for tmp in tmps:
+				obj = pickle.loads(tmp)
+				res.append(obj)
+				total = total + obj[1]
+		datas[server] = res
+		totals[server] = total
+
+		return res
+
+	def apply_value(self, server, timestamp, value):
+		global persistent
+		if(type(server) != int):
+			return
+		#server_string = "%2.2x"%(server)
+
+		if(type(timestamp) != int):
+			return
+		if(type(value) != int):
+			return
+		obj = [timestamp, value]
+		
+		if(persistent):
+			data = self.load_persistent_data(server)
+		else:
+			data = self.load_data(server)
+
+		data.append(obj)
+		totals[server] = totals[server] + obj[1]
+		total = totals[server]
+
+		if(persistent):
+			self.client.rpush(server, pickle.dumps(obj))
+		
+		last_timestamp = timestamp
+		while True:
+			obj = data[0]
+			timestamp = obj[0]
+			if(last_timestamp - timestamp > (30 * 24 * 3600)):
+				data.popleft()
+				if(persistent):
+					self.client.lpop(server)
+				totals[server] = totals[server] - obj[1]
+				total = totals[server]
+			else:
+				break
+		count = len(datas)
+		avg = total / count
+		#print server_string + '--' + str(count)
+		self.publish_avg(server, last_timestamp, avg)
+
+	def handle_request(self, req):
+		server = req[0]
+		timestamp = req[1]
+		value = req[2]
+		self.apply_value(server, timestamp, value)
+
+def child_process(q, sts):
+	while True:
+		req = q.recv()
+		sts.handle_request(req)
+
+process_num = 4
+
 if __name__ == '__main__':
+	process_list = []
+	queue_list = []
+	for i in xrange(process_num):
+		parent_conn, child_conn = Pipe()
+		p = Process(target=child_process, args=(child_conn, STS()))
+		p.start()
+		queue_list.append(parent_conn)
+		process_list.append(p)
+	#cnt = 0
 	subclient = redis.Redis(host='localhost', port=6379, db=0)
 	pubsub = subclient.pubsub()
 	pubsub.psubscribe('event')
@@ -125,7 +133,17 @@ if __name__ == '__main__':
 			continue
 		try:
 			req = json.loads(msg['data'])
-			profile()
-			handle_request(req)
+			if(type(req) != type([])):
+				continue
+			if(len(req) != 3):
+				continue
+			server = req[0]
+			if(type(server) != int):
+				continue
+			idx = server % process_num
+			queue_list[idx].send(req)
+			#threads[cnt % thread_num].enqueue(req)
+			#cnt = cnt + 1
+			#handle_request(req)
 		except Exception,e:
 			print e
--- py/test.py
+++ py/test.py
@@ -6,7 +6,7 @@ import simplejson as json
 client = redis.Redis(host='localhost', port=6379, db=0)
 
 if __name__ == '__main__':
-	for server in xrange(100000):
-		for i in xrange(3000):
+	for i in xrange(3000):
+		for server in xrange(100000):
 			client.publish('event', json.dumps([server, i, 15]))
 	
